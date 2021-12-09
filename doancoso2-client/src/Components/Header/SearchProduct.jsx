befb5c2b22eecb7ffa5dfaf5d3cb58d966540c42
thanhnguyen <nguyennhutthanhdt2000@gmail.com>
2021-12-09 14:20:28 +0700

    addthembuttom

--- doancoso2-client/src/Components/Header/SearchProduct.jsx
+++ doancoso2-client/src/Components/Header/SearchProduct.jsx
@@ -30,32 +30,38 @@ const SearchProduct = () => {
         setSuggestions(matches)
         setText(text)
     }
-
     return (
         <>
             <div style={{
                 width: '12.5pc', marginRight: '22.5pt', position: 'relative'
             }}>
-                <input type="text" placeholder="Search" className="ui-autocomplete-input"
+                <input type="text" placeholder="Tìm kiếm..." className="ui-autocomplete-input"
                     onChange={e => headlineSearch(e.target.value)}
                     style={{
                         background: ' #e5e5e5',
-                        borderRadius: '0.208333333in', width: '208px', height: '36px', border: 'none', padding: '0px 0px 0px 14px'
+                        borderRadius: '11px', width: '208px', height: '36px', border: 'none', padding: '0px 0px 0px 14px'
                     }}
                     value={text}
                 />
                 <div style={{ position: 'absolute', width: '104%', marginTop: '10px' }}>
-                    {suggestions && suggestions.map((suggestions, i) =>
-                        <div key={i} className="justify-content-md-center map-search"
-                            style={{
-                                marginBottom: '2px', background: ' #e5e5e5', borderRadius: '0.208333333in', padding: '2px 0px 2px 10px',
-                            }}
-                        >
-                            <Link to={`/DetailProduct/${suggestions.id}`}
-                                style={{ fontWeight: '500' }}
-                            ><i class="fa fa-search ml-1"></i> {suggestions.tenSP}</Link>
-                        </div>
-                    )}
+                    {
+                        suggestions && suggestions.map((suggestions, i) =>
+                            <Link to={`/DetailProduct/${suggestions.id}`}>
+                                <div key={i} className="justify-content-md-center map-search"
+                                    style={{
+                                        marginBottom: '4px', background: ' #e5e5e5', borderRadius: '11px', padding: '2px 0px 2px 10px',
+                                    }}
+                                >
+                                    <Link to={`/DetailProduct/${suggestions.id}`}
+                                        style={{ fontWeight: '500' }}
+                                    ><i class="fa fa-search ml-1"></i> {suggestions.tenSP}</Link>
+                                    <div style={{ paddingLeft: '29px' }}>
+                                        Trong:
+                                        <Link className="text-primary" to={`/Products/${suggestions.danhmucNavigation.id}`}> <i>{suggestions.danhmucNavigation.tenLoai}</i></Link>
+                                    </div>
+                                </div>
+                            </Link>
+                        )}
                 </div>
             </div>
         </>
